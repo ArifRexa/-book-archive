b8887260c0849e134c8ae471e6f393394fc923dd
ArifRexa <arif.reza3126@gmail.com>
2021-09-02 13:30:53 +0600

    finished

--- js/index.js
+++ js/index.js
@@ -1,64 +1,66 @@
 /*===============================================
-                    font 
+       Spinner & Cards Value Show Function 
 ===============================================*/
-
-const searchValue =() =>{
-    const spinnerid = document.getElementById("spinnerId")
-    spinnerid.style.display = "block"
-    const cardSectionId = document.getElementById("cards-section-id")
-    cardSectionId.style.display = "none"
-    const inputBoxText = document.getElementById("input-box")
-    const inputBox = inputBoxText.value
-    // console.log(inputBox);
-    fetch(`https://openlibrary.org/search.json?q=${inputBox}`)
-    .then(res => res.json())
-    .then(data => displayValue(data.docs))
-    inputBoxText.value = ""
+const spinnerAdd = (displayStyle) =>{
+    document.getElementById("spinnerId").style.display = displayStyle
 }
 
-const displayValue = (data) =>{
-    
+const cardValue = (displayStyle) =>{
+    document.getElementById("cards-section-id").style.display = displayStyle
+}
+
+/*===============================================
+                Click Function
+===============================================*/
+const searchValue = () =>{
+    spinnerAdd("block")
+    cardValue("none")
+    const inputBoxText = document.getElementById("input-box")  /* Get Value from Search Box */
+    const inputBox = inputBoxText.value
+    fetch(`https://openlibrary.org/search.json?q=${inputBox}`) /* Get Data from API Link */
+    .then(res => res.json())
+    .then(data => displayValue(data.docs))
+    inputBoxText.value = ""                                    /* Clear Input Field */
+}
+
+/*===============================================
+                Display Function
+===============================================*/
+const displayValue = (data) =>{    
     const totalResult = document.getElementById("total-number")
-    // totalResult.innerText = `${data.length} Results found`
     if (data.length === 0) {
         totalResult.innerHTML = `<h4>No Results Found</h4>
         <div  class="d-flex justify-content-center">
         <img src='image/nai.gif'>
-        </div>
-         `
- 
+        </div> `
     } 
     else {
-        
         totalResult.innerText = `${data.length} Results found`
     }
+
     const cardID = document.getElementById("card-id")
-    cardID.textContent = ""
-    // console.log(data.title);
-    const dataSlicing = data.slice(0,9)
+    cardID.textContent = ""                                /* Clear Results Field when searching */
+    
+    const dataSlicing = data.slice(0,9)     /* slicing total search result Display some results from total */
     dataSlicing.forEach(element => {
-        // console.log(element.title);
+        
         const div = document.createElement("div")
         div.classList.add("col")
-        // let imgee = `https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`
         div.innerHTML = `
         <div class="card h-100 shadow-lg">           
             <div class="card-body">
-            <img src= "${`https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`}" class="card-img-top"> 
-                <h5 class="card-title">${element.title}</h5>
-                <h6 class="card-title">Author: ${element.author_name? element.author_name : "NA"}</h6>
-                <h6 class="card-title">First Published Year: ${element.first_publish_year}</h6>
-                <h6 class="card-title">First Published Year: ${element.publisher}</h6>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <img src= "${`https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`}" class="card-img-top img-height"> 
+                
+                <h3 class="card-title ">${element.title}</h3>
+                <h6 class="card-title"><span class="fw-bold">Author: </span> ${element.author_alternative_name? element.author_alternative_name : element.author_name}</h6>
+                <h6 class="card-title"><span class="fw-bold">First Published Year: </span> ${element.first_publish_year? element.first_publish_year : "NA"}</h6>
+                <h6 class="card-title"><span class="fw-bold">Publisher: </span> ${element.publisher}</h6>
+                
             </div>
         </div>
         `
         cardID.appendChild(div)
     });
-    // console.log(data.length);
-    const spinnerid = document.getElementById("spinnerId")
-    spinnerid.style.display = "none"
-    const cardSectionId = document.getElementById("cards-section-id")
-    cardSectionId.style.display = "block"
-
+    spinnerAdd("none")
+    cardValue("block")
 }
